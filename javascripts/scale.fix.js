2d92db119ea109858833735c3c8c978fb81813d7
hemnath mouli <hemnathmouli@gmail.com>
2016-07-30 02:01:52 +0530

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,15 @@ function gestureStart() {
     }
   }
 }
+
+$(document).ready(function(){
+  $(document).on("click", ".btn", function (e) {
+    var type = $(this).attr("for-type");
+    $.hnotify({
+        text: "Hey there, this notification is for "+type, //Text to show in the notification
+        flag: type, //type of notification e.g success or warning, etc
+        interval: 3000 //Hide automatically after this interval 1 sec = 1000
+    });
+    e.preventDefault();
+  });
+});
