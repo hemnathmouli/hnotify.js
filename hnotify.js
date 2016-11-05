ed8c370be2bc6c489ddfdacb34f82c3de1d9ef73
hemnath mouli <hemnathmouli@gmail.com>
2016-11-05 21:49:51 +0530

    Update hnotify.js

--- hnotify.js
+++ hnotify.js
@@ -2,7 +2,7 @@
 /* Plugin Author: Hemnath Mouli                              */
 /* Plugin Name  : hnotify.js                                 */
 /* Version      : 1.0                                        */
-/* Requirements : Bootstrap ( JS )                           */
+/* Requirements : Bootstrap                                  */
 /*************************************************************/
 
 (function ($) {
